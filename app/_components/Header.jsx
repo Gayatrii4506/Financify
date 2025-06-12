ec8c71e77823a46d8c0b698a222209abff065bad
Gayatrii_4506 <gayatriduse4506@gmail.com>
2025-06-12 17:52:51 +0530

    Header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,37 @@
-'use client'
+"use client";
 
-import React from "react"
-import Image from 'next/image'
-import {Button} from "@/components/ui/button"
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+function Header() {
+  const { user, isSignedIn } = useUser();
+
+  return (
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      {/* Left: Logo + Title */}
+      <div className="flex flex-row items-center">
+        <Image src={"/donut-chart.svg"} alt="logo" width={40} height={25} />
+        <span className="text-blue-800 font-bold text-xl ml-2">Financify</span>
+      </div>
+
+      {/* Right: Auth or Buttons */}
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Link href="/dashboard">
+            <Button variant="outline" className="rounded-full">Dashboard</Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button className="rounded-full" color="blue-800">Get Started</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
